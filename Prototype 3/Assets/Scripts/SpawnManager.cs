6d0a14fc736a6eac90339666b6297cd4126b9a8a
marienams <mhmsngh0@gmail.com>
2020-07-30 13:21:55 +0530

    Based on prototype 3 of create with code. Adding more feature and levels to make it more personalised and unique.

---
+++ Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] obstaclePrefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("spawnObstacle", 2f, 1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void spawnObstacle()
+    {
+        int obstacleIndex = Random.Range(0, obstaclePrefabs.Length);
+        Vector3 spawnPos = new Vector3(35, 0, 0);
+        Instantiate(obstaclePrefabs[obstacleIndex], spawnPos, obstaclePrefabs[obstacleIndex].transform.rotation);
+    }
+}
